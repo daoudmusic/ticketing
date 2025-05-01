7e97b49896c04a2bc64936a9635497698a815ed7
daoudmusic <contact@daoudmusic.Com>
2025-05-01 18:22:50 +0200

    Create events.ts

---
+++ lib/events.ts
@@ -0,0 +1,18 @@
+export const events = [
+  {
+    id: "bikini-nov12",
+    name: "daoud live @ Le Bikini",
+    date: "November 12, 2025",
+    price: 20,
+    slug: "bikini-nov12",
+    image: "/daoud-event-default.jpg",
+  },
+  {
+    id: "metronum-may23",
+    name: "daoud live @ Le Metronum",
+    date: "May 23, 2025",
+    price: 15,
+    slug: "metronum-may23",
+    image: "/daoud-event-default.jpg",
+  },
+];
